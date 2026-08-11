bdb822b6bf53ae2a85f400f2d317314218eb7aa1
Jessica Zeng <szen566@aucklanduni.ac.nz>
2026-08-12 00:36:53 +1200

    started planning dirichlet model

---
+++ simulations/dirichletmodel.py
@@ -0,0 +1,53 @@
+# ============================================================
+# Dirichlet Catch Model
+# How it works:
+#   1. Predict the total catch for the year, then
+#   2. Model the composition of that total catch across stations using historical proportions.
+# ============================================================
+
+# ============================================================
+# Function 1: Construct year x Station catch table
+# ============================================================
+
+
+# ============================================================
+# Function 2: Calculate total catch for each year
+# ============================================================
+
+
+# ============================================================
+# Function 2: Calculate each station's proportions
+# ============================================================
+
+# ============================================================
+# Function 4: Create historical composition matrix
+# Each row sum to 1, each column is a station
+# ============================================================
+
+
+# ============================================================
+# Function 5: Summarise station proportions 
+# Calc for each station: mean, std, and 95% CI of proportions across years, min, max etc.
+# ============================================================
+
+# ============================================================
+# Function 6 & 7 & 8: Visualise proportions (overtime, mean, distribution of proportions)
+# ============================================================
+
+# ============================================================
+# Function 9: Estimate Dirichlet params & Fit model 
+# ============================================================
+
+
+# ============================================================
+# Function 10: Generate simulated station proportions 
+# ============================================================
+
+
+# ============================================================
+# Function 11: Predict total catch (this year)
+# ============================================================
+
+# ============================================================
+# Function 12: Combine total catch & proportions to get estimated catch per station
+# ============================================================
